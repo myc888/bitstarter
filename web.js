63924b42687f569d0be25ef5696b7b6226bec337
Mick Mor <myc@youssefmir.com>
2013-07-08 15:33:39 +0000

    Updated web.js to point to index.html

--- web.js
+++ web.js
@@ -1,3 +1,31 @@
+var fs = require('fs');
+
+fs.readFileSync(filename, function (err, data) {
+  if (err) throw err;
+/*  console.log(data);*/
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(data.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+
+
+
+
+
+});
+
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());

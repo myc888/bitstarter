c5bdbea33f48272e48cc7fc8ddc7f175fdb82895
Mick Mor <myc@youssefmir.com>
2013-07-09 15:16:57 +0000

    Updated web.js with readFileSync

--- web.js
+++ web.js
@@ -1,37 +1,17 @@
 var fs = require('fs');
 
-fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-/*  console.log(data);*/
-
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var file_data;
+var file_string;
+
+file_data = fs.readFileSync('./index.html');
+console.log(file_data.toString());
+
+
 
 app.get('/', function(request, response) {
-  response.send(data.toString());
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-
-
-
-
-
-});
-
-
-
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(file_data.toString());
 });
 
 var port = process.env.PORT || 5000;
